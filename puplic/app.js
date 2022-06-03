54134739a664754647ed44120dce9c097da30424
Ayman-M-Ali <aymanmalawyer2@gmail.com>
2022-06-03 19:53:46 +0300

    Portfolio

---
+++ puplic/app.js
@@ -0,0 +1,11 @@
+// let section = document.querySelector("#skills");
+// let spans = documnet.querySelectorAll(".skill span");
+
+// window.onscroll = function () {
+//   if (window.scrollY >= section.offsetTop) {
+//     console.log("Ok");
+//     spans.forEach((span) => {
+//       span.style.width = span.dataset.width;
+//     })
+//   }
+// };
